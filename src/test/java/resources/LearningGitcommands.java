30f0bda6cfc8571e337d18ea15c7c1e7004e2bdd
rahul_API <drahul171995@gmail.com>
2025-08-03 12:06:53 +0530

    commits on Develop branch

--- src/test/java/resources/LearningGitcommands.java
+++ src/test/java/resources/LearningGitcommands.java
@@ -5,6 +5,9 @@ public class LearningGitcommands {
 	public void learningGitCommands() {
 		
 		System.out.println("git Commands");
+		System.out.println("New Testing git Commands");
+		System.out.println("New Developing git Commands");
+		System.out.println("New Deploying git Commands");
 	}
 
 }
